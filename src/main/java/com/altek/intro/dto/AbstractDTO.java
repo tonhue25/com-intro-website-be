effffa09ea28bbe353d612b0455972b85de85b8f
ITVuNguyen <it.vunguyen.vidal@gmail.com>
2021-05-01 22:20:33 +0700

    clean code

--- src/main/java/com/altek/intro/dto/AbstractDTO.java
+++ src/main/java/com/altek/intro/dto/AbstractDTO.java
@@ -1,39 +1,15 @@
 package com.altek.intro.dto;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class AbstractDTO {
-
     private Long id;
     private int activeFlag;
+    private String createBy;
     private Date createDate;
-    private Date updateDate;
-
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public int getActiveFlag() {
-        return activeFlag;
-    }
-    public void setActiveFlag(int activeFlag) {
-        this.activeFlag = activeFlag;
-    }
-    public Date getCreateDate() {
-        return createDate;
-    }
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
+    private String modifyBy;
+    private Date modifyDate;
 }
-
--- src/main/java/com/altek/intro/entites/AbstractEntity.java
+++ src/main/java/com/altek/intro/entites/AbstractEntity.java
@@ -8,48 +8,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
 
 @MappedSuperclass
+@Data
 public class AbstractEntity {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @Column(name="activeflag")
+    @Column(name="active_flag")
     private int activeFlag;
-    @Column(name="createdate")
+    @Column(name="create_date")
     private Date createDate;
-    @Column(name="updatedate")
-    private Date updateDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public int getActiveFlag() {
-        return activeFlag;
-    }
-
-    public void setActiveFlag(int activeFlag) {
-        this.activeFlag = activeFlag;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    @Column(name="modify_by")
+    private String modifyBy;
+    @Column(name="modify_date")
+    private Date modifyDate;
 }
