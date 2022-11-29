ac9c181c485f04e7f951df43740f416d0421a6e4
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2022-11-29 13:45:25 +0700

    news repo

--- src/main/java/com/altek/intro/repository/NewsRepository.java
+++ src/main/java/com/altek/intro/repository/NewsRepository.java
@@ -10,10 +10,4 @@ import java.util.Optional;
 public interface NewsRepository extends AbstractRepository<News, Long> {
     @Query(value = "select u from News u where u.status = 1 and u.id = :id")
     Optional<News> findById(Long id);
-
-    @Query("select e from News e where  e.status = 1 and (:search is null or " +
-            "   ( lower(e.title) like lower(concat(:search, '%'))  or " +
-            "   lower(e.shortDescription) like lower(concat(:search, '%')) )) ")
-            List<News> getAll(@Param("search") String search);
-
 }
